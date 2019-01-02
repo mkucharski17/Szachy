fb5e7c127f8d7a3efd1d7b439ef3d5d5230e26b7
Michał <m.kucharski17@wp.pl>
2019-01-02 15:29:51 +0100

    first commit

---
+++ chessPiece.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michal on 02.01.2019.
+//
+
+#include "chessPiece.h"
---
+++ chessPiece.h
@@ -0,0 +1,14 @@
+//
+// Created by Michal on 02.01.2019.
+//
+
+#ifndef LAB3_CHESSPIECE_H
+#define LAB3_CHESSPIECE_H
+
+
+class chessPiece {
+
+};
+
+
+#endif //LAB3_CHESSPIECE_H
