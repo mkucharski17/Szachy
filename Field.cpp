fb03e034a9c46a144b195c5dfde3662865db39de
Michał <m.kucharski17@wp.pl>
2019-01-07 17:31:09 +0100

    added class field instead of struct field

---
+++ Field.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Michal on 07.01.2019.
+//
+
+#include "Field.h"
+
+string & Field::getChessPieceName() {
+    return chesPieceName;
+}
---
+++ Field.h
@@ -0,0 +1,24 @@
+//
+// Created by Michal on 07.01.2019.
+//
+
+#ifndef LAB3_FIELD_H
+#define LAB3_FIELD_H
+
+#include <string>
+
+using namespace std;
+
+
+
+
+class Field {
+    string chesPieceName = "empty";
+
+public:
+    string & getChessPieceName();
+
+};
+
+
+#endif //LAB3_FIELD_H
