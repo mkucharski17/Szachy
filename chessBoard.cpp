27a025ab7e67a5af1771d68d1b652d388bd351e1
Michał <m.kucharski17@wp.pl>
2019-01-08 13:03:38 +0100

    creating setting chess Pieces

--- chessBoard.cpp
+++ chessBoard.cpp
@@ -2,7 +2,16 @@
 // Created by Michal on 03.01.2019.
 //
 
+#include <iostream>
 #include "chessBoard.h"
+#include "Bishop.h"
+#include "Pawn.h"
+#include "King.h"
+#include "Queen.h"
+#include "Knight.h"
+#include "Rook.h"
+
+enum colours{white,black};
 
 chessBoard::chessBoard() {
     for(int i = 0 ; i < 8 ; i++)
@@ -13,4 +22,73 @@ chessBoard::chessBoard() {
 
 Field& chessBoard::getBoard(int X , int Y) {
     return board[X][Y];
+}
+
+void chessBoard::setPawns() {
+
+    for(int i = 0 ;  i < 8 ; i++){
+        chessPiece*ch = new Pawn(white);
+        board[1][i].setChessMan(ch);
+        ch = new Pawn(black);
+        board[6][i].setChessMan(ch);
+    }
+}
+
+void chessBoard::setKings() {
+
+    for(int i = 0 ;  i < 8 ; i++){
+        chessPiece*ch = new King(white);
+        board[0][4].setChessMan(ch);
+        ch = new King(black);
+        board[7][4].setChessMan(ch);
+    }
+}
+
+void chessBoard::setQueens() {
+
+    for(int i = 0 ;  i < 8 ; i++){
+        chessPiece*ch = new Queen(white);
+        board[0][3].setChessMan(ch);
+        ch = new Queen(black);
+        board[7][3].setChessMan(ch);
+    }
+}
+
+void chessBoard::setBishops() {
+
+    for(int i = 2 ;  i < 8 ; i+=3){
+        chessPiece*ch = new Bishop(white);
+        board[0][i].setChessMan(ch);
+        ch = new Bishop(black);
+        board[7][i].setChessMan(ch);
+    }
+}
+
+void chessBoard::setKnights() {
+
+    for(int i = 1 ;  i < 8 ; i+=5){
+        chessPiece*ch = new Knight(white);
+        board[0][i].setChessMan(ch);
+        ch = new Knight(black);
+        board[7][i].setChessMan(ch);
+    }
+}
+
+void chessBoard::setRooks() {
+
+    for(int i = 0 ;  i < 8 ; i+=7){
+        chessPiece*ch = new Rook(white);
+        board[0][i].setChessMan(ch);
+        ch = new Rook(black);
+        board[7][i].setChessMan(ch);
+    }
+}
+
+void chessBoard::setBoard() {
+    setPawns();
+    setKings();
+    setQueens();
+    setBishops();
+    setRooks();
+    setKnights();
 }
--- chessBoard.h
+++ chessBoard.h
@@ -13,10 +13,19 @@ using namespace std;
 
 class chessBoard {
     Field board[8][8];
+    void setPawns();
+    void setKings();
+    void setQueens();
+    void setBishops();
+    void setRooks();
+    void setKnights();
 
 public:
     chessBoard();
     Field& getBoard(int X ,  int Y);
+    void setBoard();
+
+
 
 };
 
