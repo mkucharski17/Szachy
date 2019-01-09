62f2ccbbc270db9ab050c0f8f9fbc4ab3ce5ea51
Michał <m.kucharski17@wp.pl>
2019-01-09 15:36:51 +0100

    move methods to rules checker

--- chessBoard.cpp
+++ chessBoard.cpp
@@ -16,13 +16,11 @@ enum colours {
     white, black
 };
 
-
 chessBoard::chessBoard() {
     for (int i = 0; i < 8; i++)
         for (int j = 0; j < 8; j++)
             board[i][j] = Field();
 }
-
 
 Field &chessBoard::getBoard(int X, int Y) {
     return board[X][Y];
@@ -98,7 +96,6 @@ void chessBoard::show() {
                 cout << board[j][i].getChessMan()->getName();
                 cout << board[j][i].getChessMan()->getColour() << "(" << j << "," << i << ")" << "  ";
             } else cout << "empty" << "(" << j << "," << i << ")" << "  ";
-
         }
         cout << endl;
     }
@@ -110,95 +107,3 @@ void chessBoard::move(vertexes from, vertexes to) {
     board[from.X][from.Y].setChessMan(nullptr);
 }
 
-vector<vertexes> chessBoard::checkMove(int x, int y) {
-    vector<vertexes> possibilities;
-    if (board[x][y].getChessMan()->getName() == "Pawn") {
-        checkPawnMove(possibilities, x, y);
-    }
-
-    else if (board[x][y].getChessMan()->getName() == "Knight") {
-        checkKnightMove(possibilities, x, y);
-    }
-
-    vector<vertexes>::iterator iterator;
-
-    for (iterator = possibilities.begin(); iterator != possibilities.end(); ++iterator) {
-        cout << iterator->X << " " << iterator->Y << endl;
-    }
-}
-
-void chessBoard::checkPawnMove(vector<vertexes> &possibilities, int x, int y) {
-    vertexes temp;
-    int shift;
-
-    if (board[x][y].getChessMan()->getColour() == white)
-        shift = 1;
-
-    else shift = -1;
-
-
-    //classic move
-    if ((y < 7 && shift == 1) || (y > 0 && shift == -1))
-        if (board[x][y + shift].getChessMan() == nullptr) {
-            temp.X = x;
-            temp.Y = y + shift;
-            possibilities.push_back(temp);
-        }
-
-    //double move
-    if ((y == 1 && shift == 1) || (y == 6 && shift == -1))
-        if (board[x][y + 2 * shift].getChessMan() == nullptr && board[x][y + shift].getChessMan() == nullptr) {
-
-            temp.X = x;
-            temp.Y = y + 2 * shift;
-            possibilities.push_back(temp);
-        }
-
-    //right attack
-    if ((x < 7 && y < 7 && shift == 1) || (x > 0 && y > 0 && shift == -1))
-        if (board[x + shift][y + shift].getChessMan() != nullptr &&
-            board[x + shift][y + shift].getChessMan()->getColour() != board[x][y].getChessMan()->getColour()) {
-
-            temp.X = x + shift;
-            temp.Y = y + shift;
-            possibilities.push_back(temp);
-        }
-
-    //left attack
-    if ((x > 0 && y < 7 && shift == 1) || (x < 7 && y > 0 && shift == -1))
-        if (board[x - shift][y + shift].getChessMan() != nullptr &&
-            board[x - shift][y + shift].getChessMan()->getColour() != board[x][y].getChessMan()->getColour()) {
-
-            temp.X = x - 1;
-            temp.Y = y + 1;
-            possibilities.push_back(temp);
-        }
-
-}
-
-void chessBoard::checkKnightMove(vector<vertexes> &possibilities, int x, int y) {
-    vertexes temp;
-    for (int i = -2; i < 3; i += 4)
-        for (int j = -1; j < 2; j += 2) {
-            temp.X = x + i;
-            temp.Y = y + j;
-            if(inBoard(temp))
-                possibilities.push_back(temp);
-        }
-
-    for (int i = -1; i < 2; i += 2)
-        for (int j = -2; j < 3; j += 4) {
-            temp.X = x + i;
-            temp.Y = y + j;
-            if(inBoard(temp))
-                possibilities.push_back(temp);
-
-        }
-}
-
-bool chessBoard::inBoard(vertexes v) {
-    if(v.X < 0 || v.X > 7 || v.Y < 0 || v.Y > 7)
-        return false;
-
-    else return true;
-}
--- chessBoard.h
+++ chessBoard.h
@@ -18,15 +18,13 @@ struct vertexes {
 
 class chessBoard {
     Field board[8][8];
-    bool inBoard(vertexes v);
     void setPawns();
     void setKings();
     void setQueens();
     void setBishops();
     void setRooks();
     void setKnights();
-    void checkPawnMove(vector<vertexes>& possibilities, int x, int y);
-    void checkKnightMove(vector<vertexes> &possibilities, int x, int y);
+
 
 
 
@@ -37,11 +35,6 @@ public:
     void setBoard();
     void move(vertexes from, vertexes to);
     void show();
-    vector<vertexes> checkMove(int x, int y);
-
-
-
-
 };
 
 
